481f0e308163d7d7efe9884228bc08970d4cb66f
brian sauro <bhannasauro@hotmail.com>
2020-12-16 08:43:22 -0500

    Added TitleScreen Scene Added GameManager class Added Start Button Added Quit Button Added Text Mesh Pro assets

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+namespace Assets.Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        
-    }
+        private enum GameState
+        {
+            Title,
+            NewGame,
+            LoadGame,
+            Travel,
+            Combat,
+            Encounter,
+            GameOver,
+            Quit
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        private const string TitleScreenSceneName = "TitleScreen";
+        private const string TravelSceneName = "Travel";
+        private const string CombatSceneName = "Combat";
+
+        private Scene _currentScene;
+
+        private List<string> _subscribedEventNames;
+
+        private List<GameObject> _activeWindows;
+
+        private GameState _currentState;
+
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
