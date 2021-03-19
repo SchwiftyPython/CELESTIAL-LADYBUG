f68aa264f960451a17a4888bfa446246cd3a14f4
brian sauro <bhannasauro@hotmail.com>
2021-03-19 16:51:34 -0400

    Recolored some portraits. Seperated portraits into layers Made a simple portrait generator.

---
+++ Assets/Scripts/SpriteStore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Assets.Scripts.UI;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts
+{
+    public class SpriteStore : MonoBehaviour
+    {
+        //todo dictionaries for each slot type
+
+        //for testing
+        public Sprite[] SkinSprites;
+        public Sprite[] EarSprites;
+        public Sprite[] ChestSprites;
+        public Sprite[] FacialFairSprites;
+        public Sprite[] HairSprites;
+        public Sprite[] HelmetSprites;
+
+        public static SpriteStore Instance;
+
+        private void Start()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+
+            //todo initialize dictionaries
+        }
+
+        public Sprite GetRandomSpriteForSlot(Portrait.Slot slot)
+        {
+            switch (slot)
+            {
+                case Portrait.Slot.Skin:
+                    return SkinSprites[Random.Range(0, SkinSprites.Length)];
+                case Portrait.Slot.Ears:
+                    return EarSprites[Random.Range(0, EarSprites.Length)]; 
+                case Portrait.Slot.Chest:
+                    return ChestSprites[Random.Range(0, ChestSprites.Length)];
+                case Portrait.Slot.FacialHair:
+                    return FacialFairSprites[Random.Range(0, FacialFairSprites.Length)];
+                case Portrait.Slot.Hair:
+                    return HairSprites[Random.Range(0, HairSprites.Length)];
+                case Portrait.Slot.Helmet:
+                    return HelmetSprites[Random.Range(0, HelmetSprites.Length)];
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(slot), slot, null);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Portrait.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.UI
+{
+    public class Portrait : MonoBehaviour
+    {
+        public enum Slot
+        {
+            Skin, 
+            Ears,
+            Chest,
+            FacialHair,
+            Hair,
+            Helmet
+        }
+
+        [SerializeField] private GameObject _skin;
+        [SerializeField] private GameObject _ears;
+        [SerializeField] private GameObject _chest;
+        [SerializeField] private GameObject _facialHair;
+        [SerializeField] private GameObject _hair;
+        [SerializeField] private GameObject _helmet;
+
+        public void SetSprite(Slot slot, Sprite sprite)
+        {
+            switch (slot)
+            {
+                case Slot.Skin:
+                    _skin.GetComponent<Image>().sprite = sprite;
+                    break;
+                case Slot.Ears:
+                    _ears.GetComponent<Image>().sprite = sprite;
+                    break;
+                case Slot.Chest:
+                    _chest.GetComponent<Image>().sprite = sprite;
+                    break;
+                case Slot.FacialHair:
+                    _facialHair.GetComponent<Image>().sprite = sprite;
+                    break;
+                case Slot.Hair:
+                    _hair.GetComponent<Image>().sprite = sprite;
+                    break;
+                case Slot.Helmet:
+                    _helmet.GetComponent<Image>().sprite = sprite;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(slot), slot, null);
+            }
+        }
+
+        public void MakeRandomPortrait()
+        {
+            const int helmetChance = 47;
+
+            if (Random.Range(0, 101) > helmetChance)
+            {
+                _helmet.SetActive(true);
+            }
+            else
+            {
+                _helmet.SetActive(false);
+            }
+
+            foreach (Slot slot in Enum.GetValues(typeof(Slot)))
+            {
+                var sprite = SpriteStore.Instance.GetRandomSpriteForSlot(slot);
+
+                SetSprite(slot, sprite);
+            }
+        }
+    }
+}
